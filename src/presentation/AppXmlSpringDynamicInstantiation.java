0a16a7ef05e5ec13128c8b30d95b818853b3d882
nnyimc <nmncls@orange.fr>
2020-09-11 23:59:10 +0200

    Dynamic instantiation - Config.txt OK Dynamic instantiation - springIoc.xml OK (needs to be placed in /src)

--- src/presentation/AppXmlSpringDynamicInstantiation.java
+++ src/presentation/AppXmlSpringDynamicInstantiation.java
@@ -8,9 +8,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class AppXmlSpringDynamicInstantiation {
     public static void main(String[] args) {
         int[] intArray = {-8,4,0,-2,7,6,-1,5,1,-9,-5,-5,-8,-7,4,4,1,1,0,2};
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring-ioc.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("springIoc.xml");
         ISort sort = (ISort) context.getBean("srt");
-
         ISearch search = (ISearch) context.getBean("srch");
         if (search.searchInt(-11, sort.sortArray(intArray))) {
             System.out.println("Your value has been found!");
